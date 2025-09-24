c1063541e8d9030cd319f4a84bb80b17de771fc8
Kubikula123 <tianshuchang666@gmail.com>
2025-09-24 09:23:54 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num;
+Console.WriteLine("Enter a number:");
+num = int.Parse(Console.ReadLine());
+
+for (int i = 1;i <= num; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.Write("Fizz Buzz, ");
+    }
+    else if (i % 3 == 0) 
+    {
+        Console.Write("Fizz, ");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.Write("Buzz, ");
+    }
+    else
+    {
+        Console.Write(i + ", ");
+    }
+}
